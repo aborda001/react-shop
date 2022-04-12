a64ef948478171c112ba77af41e4161798f0df9e
Arnaldo Borda <aborda001@gmail.com>
2022-04-12 00:10:02 -0400

    Creating basic routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            React shop
+        </div>
+    );
+}
+
+export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     mode: 'development',
     resolve: {
@@ -54,5 +55,6 @@ module.exports = {
         },
         compress: true,
         port: 3005,
+        historyApiFallback: true,
     }
 }
