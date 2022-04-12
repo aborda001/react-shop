48165cf2e4260ab9f29f2d2b5c29d5d096faf5fe
Arnaldo Borda <aborda001@gmail.com>
2022-04-12 00:49:59 -0400

    All files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ProductList from '../containers/ProductList';
 
 const Home = () => {
-    return (
-        <div>
-            React shop
-        </div>
-    );
+	return (
+		<>
+			<ProductList />
+		</>
+	);
 }
 
 export default Home;
